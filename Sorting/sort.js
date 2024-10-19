3470d474baee802c941bfa31a88e0b188d91deb7
Ashish Bhusal <75556275+ashishbhusal1@users.noreply.github.com>
2024-10-19 09:32:08 +0545

    Create sort.js

---
+++ Sorting/sort.js
@@ -0,0 +1,16 @@
+const words = [
+    "JavaScript",
+    "Program",
+    "to",
+    "Sort",
+    "Words",
+    "in",
+    "Alphabetical",
+    "Order",
+];
+
+// Sorting the input array using array.sort
+const sortedWords = words.sort();
+
+//Getting the sorter array output
+console.log(sortedWords);
